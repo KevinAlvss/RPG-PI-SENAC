9e9e221ba8561cb93c0882b5e9c25d08578a6f2d
kevinalvss <kevin.bernardo64@gmail.com>
2022-04-17 12:25:24 -0300

    nova função "maquina de escrever"

--- Client/Opcoes.java
+++ Client/Opcoes.java
@@ -1,6 +1,6 @@
 package Client;
 import Cenas.*;
-
+import Utils.Utils;
 public class Opcoes {
 
     public static void iniciarJogo() {
@@ -8,7 +8,7 @@ public class Opcoes {
     }
 
     public static void instrucoes() {
-        System.out.println("\nO jogo se passa na época de 1939, durante a 2ª guerra mundial, onde você terá que ajudar Alan Turing a resolver o Enigma. \n"
+        Utils.escrever("\nO jogo se passa na época de 1939, durante a 2ª guerra mundial, onde você terá que ajudar Alan Turing a resolver o Enigma. \n"
         + "Uma máquina que foi desenvolvida para criptografar coordenadas e assim, efetuar ataques sem que ninguém saiba onde irão acontecer. \n"
         + "Você terá escolhas a fazer e dependendo delas, isso influenciará no futuro da guerra.\n");
     }
---
+++ Utils/Utils.java
@@ -0,0 +1,28 @@
+package Utils;
+
+public class Utils {
+    public static void reticencias(int qntPontinhos) {
+        try {
+            for (int i = 0; i < qntPontinhos; i++) {
+                Thread.sleep(700);
+                System.out.print(".");
+                Thread.sleep(700);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public static void escrever(String conteudo) {
+        try {
+            for (int i = 0; i < conteudo.length(); i++) {
+                Thread.sleep(45);
+                System.out.print(conteudo.charAt(i));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
