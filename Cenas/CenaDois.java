05ed448df4fc63294388f95f319f86dba10fcce2
Furuzawa <affonso97@icloud.com>
2022-04-25 22:07:31 -0300

    Cena dois inciada

--- Cenas/CenaDois.java
+++ Cenas/CenaDois.java
@@ -1,9 +1,74 @@
 package Cenas;
 
+import java.util.Scanner;
+import Utils.Utils;
+
 public class CenaDois {
 
     public static void initCenaDois() {
-
+        System.out.println("\nUma semana depois...\n");
+        Utils.escrever(
+                "\nApós mais um dia de trabalho eu decido ir dormir... zzz... zzz... zzz... E percebo que estou sonhando com o dia em que conheci o Alan. Eu estava sentado no refeitório da escola junto com o meu primo Christopher e vejo um garoto separando os alimentos por cores... O Christopher então, decide ir ao patio, quando de repente 5 garotos jogam o prato de comida de Alan para cima e começam a zombar com ele.\n");
+        Utils.escrever(
+                "\nAlan decide sair correndo para se livrar dos agressores\n");
+        comoConheciOAlan();
     }
 
+    public static void comoConheciOAlan() {
+
+        boolean saiu = false;
+
+        while (!saiu) {
+
+            System.out.println("\n1 - Corro atrás dos agressores para ajudar Alan\n2 - Não faço nada\n");
+
+            Scanner sc = new Scanner(System.in);
+
+            String decisao = sc.next();
+
+            switch (decisao) {
+                case "1":
+                    corroAtrasDosAgressoresParaAjudarAlan();
+                    saiu = true;
+                    break;
+                case "2":
+                    naoFacoNada();
+                    saiu = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public static void corroAtrasDosAgressoresParaAjudarAlan() {
+        Utils.escrever(
+                "\nEu persigo eles até pararem em uma sala e começarem a bater nele\n");
+        chamarChristopher();
+    }
+
+    public static void naoFacoNada() {
+        Utils.escrever(
+                "\nO sinal do intervalo bate e vou para minha sala e vejo os agressores batendo no Alan\n");
+        chamarChristopher();
+    }
+
+    public static void chamarChristopher() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n1 - Chamar Christopher.\n");
+
+        String decisao = sc.next();
+
+        switch (decisao) {
+            case "1":
+                Utils.escrever(
+                        "\nO Christopher chega e bate nos agressores e salva Alan\n");
+                Utils.escrever(
+                        "\nPercebo que acordo assustado com tudo que sonhei\n");
+            default:
+                break;
+        }
+    }
 }
