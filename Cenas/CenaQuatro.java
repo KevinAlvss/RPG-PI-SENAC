bfb69df9d2de37e1eefc9e87f205899b511b35eb
Furuzawa <affonso97@icloud.com>
2022-05-02 23:16:53 -0300

    Alterações-feitas-com-"\n"

--- Cenas/CenaQuatro.java
+++ Cenas/CenaQuatro.java
@@ -12,17 +12,17 @@ public class CenaQuatro {
     public static void initCenaQuatro() {
         Utils.escrever(
                 "A ansiedade não poderia ser pior... Já fazem 4 dias que fui demitido... Não necessariamente pelo atraso, mas sim por falta de recursos... Faltava dinheiro... Faltava pessoas....  E o dono do escritório aonde trabalho dispensou todos, pois já não havia mais condições de manter os funcionários, agora minha única chace é aquela carta....\n"
-                        + "....\n"
-                        + "....\n"
-                        + "*som de metal*\n"
-                        + "Parece que alguém mexeu na minha caixa de correios, então decido ir ate lá ver o que era.\n"
-                        + "O carteiro acabou de sair da frente de minha casa. Então fui ansioso ver e haviam 2 cartas... Uma era do meu antigo chefe e a outro era uma carta que estava escrito “Oportunidade de emprego” que deduzi ser aquela do jornal.\n");
+                        + "\n....\n"
+                        + "\n....\n"
+                        + "\n*som de metal*\n"
+                        + "\nParece que alguém mexeu na minha caixa de correios, então decido ir ate lá ver o que era.\n"
+                        + "\nO carteiro acabou de sair da frente de minha casa. Então fui ansioso ver e haviam 2 cartas... Uma era do meu antigo chefe e a outro era uma carta que estava escrito “Oportunidade de emprego” que deduzi ser aquela do jornal.");
 
         boolean saiu = false;
 
         while (!saiu) {
             System.out.println(
-                    "1 - Abrir carta do ex chefe Sr.George.\n");
+                    "\n1 - Abrir carta do ex chefe Sr.George.\n");
 
             String decisao = sc.next();
 
@@ -39,13 +39,13 @@ public class CenaQuatro {
 
     public static void cartaChefe() {
         Utils.escrever("'Olá caro(a) " + Jogador.GetNomeJogador() + ", Como vai?\n"
-                + "Sei que aquilo foi muito repentino, peço minhas sinceras desculpas pelo o que aconteceu, mas infelizmente eu não estava mais conseguindo sustentar as coisas, até na minha casa a situação está muito difícil. Como eu te considero uma pessoa de ouro eu venho te oferecer uma oportunidade, uma passagem só de ida para o Brazil, meu irmão foi para lá a alguns anos e conhece um rapaz chamado Roberto Marinho que está com uma ideia de um novo negócio por lá e como você é uma pessoa de ouro decidi que te levaria comigo nessa jornada.... Caso esteja interessado me encontre na frente do nosso antigo escritório amanhã as 14:00Hrs.'\n");
+                + "\nSei que aquilo foi muito repentino, peço minhas sinceras desculpas pelo o que aconteceu, mas infelizmente eu não estava mais conseguindo sustentar as coisas, até na minha casa a situação está muito difícil. Como eu te considero uma pessoa de ouro eu venho te oferecer uma oportunidade, uma passagem só de ida para o Brazil, meu irmão foi para lá a alguns anos e conhece um rapaz chamado Roberto Marinho que está com uma ideia de um novo negócio por lá e como você é uma pessoa de ouro decidi que te levaria comigo nessa jornada.... Caso esteja interessado me encontre na frente do nosso antigo escritório amanhã as 14:00Hrs.'");
 
         boolean saiu = false;
 
         while (!saiu) {
             System.out.println(
-                    "1 - Ler carta de oportunidade de emprego.\n");
+                    "\n1 - Ler carta de oportunidade de emprego.\n");
 
             String decisao = sc.next();
 
@@ -61,14 +61,14 @@ public class CenaQuatro {
     }
 
     public static void cartaAnuncio() {
-        Utils.escrever("'Olá caro(a), " + Jogador.GetNomeJogador() + " .\n"
-                + "venho por meio desta carta lhe informar que você passou em nosso processo seletivo, porém agora temos uma última fase eliminatória, venha amanhã a prefeitura de Richmond as 14:00hrs.'\n");
+        Utils.escrever("'Olá caro(a), " + Jogador.GetNomeJogador() + ".\n"
+                + "\nVenho por meio desta carta lhe informar que você passou em nosso processo seletivo, porém agora temos uma última fase eliminatória, venha amanhã a prefeitura de Richmond as 14:00hrs.'\n");
 
         boolean saiu = false;
 
         while (!saiu) {
             System.out.println(
-                    "1 - Aceitar proposta do chefe.\n2 - Aceitar proposta de emprego.\n");
+                    "\n1 - Aceitar proposta do chefe.\n2 - Aceitar proposta de emprego.\n");
 
             String decisao = sc.next();
 
@@ -90,8 +90,8 @@ public class CenaQuatro {
     public static void aceitarOportunidade1() {
         Utils.escrever(
                 "Estou ansioso para conversar com o Sr.George, estava no nosso ponto de encontro as 13:00hrs, uma hora mais cedo que o combinado. Às 13:30 vejo o Sr.George se aproximando.....\n"
-                        + "Ele chega até mim e me convida para entrarmos em nosso antigo escritório... Lá ele me explica com detalhes sobre o negócio que o irmão dele estava envolvido com o Sr.Roberto e que lá iriamos iniciar novas vidas, porém com nomes diferentes, pois em tempos de guerra nós deveríamos ter nos juntado ao exército.... O Sr.George me diz que iríamos sair dali uma semana, esse era o tempo que eu tinha para vender todos meus pertences que iriam ficar para trás....\n"
-                        + "Consegui. Foi difícil, porém eu consegui vender tudo que tinha e em tempo de guerra é dificil lucrar com isso.... A partir daqui serão pessoas novas, país novo, idioma novo, vida nova.\n");
+                        + "\nEle chega até mim e me convida para entrarmos em nosso antigo escritório... Lá ele me explica com detalhes sobre o negócio que o irmão dele estava envolvido com o Sr.Roberto e que lá iriamos iniciar novas vidas, porém com nomes diferentes, pois em tempos de guerra nós deveríamos ter nos juntado ao exército.... O Sr.George me diz que iríamos sair dali uma semana, esse era o tempo que eu tinha para vender todos meus pertences que iriam ficar para trás....\n"
+                        + "\nConsegui. Foi difícil, porém eu consegui vender tudo que tinha e em tempo de guerra é dificil lucrar com isso.... A partir daqui serão pessoas novas, país novo, idioma novo, vida nova.");
 
         Utils.escrever(
                 "A guerra acabou em 1955 com a total destruição da Alemanha e o que era o território alemão, agora não passa de um enorme cemitério. Foi uma guerra muito difícil, após as bombas de Hiroshima e Nagasaki.\n Em 45 o império japonês ficou cada vez mais irritado fazendo com que as pesquisas em armas de destruição em massa foram aceleradas, tanto no japão quanto na Alemanha.");
@@ -106,16 +106,16 @@ public class CenaQuatro {
     public static void aceitarOportunidade2() {
         Utils.escrever(
                 "Enviei uma carta para o Sr.George agradecendo toda sua consideração e dizendo que eu recusaria a proposta, e disse que não poderia ir, pois estava sentindo que algo maior estava por acontecer...\n"
-                        + "Então fiz o que solicitava na carta, e passei o dia ansioso para ver oque iria acontecer\n"
-                        + "...\n"
-                        + "Acordei as 6 horas da manhã pois eu não aguentava de ansiedade e fui arrumar a casa para me distrair, porém fiquei a todo momento de olhos no relógio.\n"
-                        + "Já era por volta das 8:30 e minha barriga gritava por comida, então...\n");
+                        + "\nEntão fiz o que solicitava na carta, e passei o dia ansioso para ver oque iria acontecer\n"
+                        + "\n...\n"
+                        + "\nAcordei as 6 horas da manhã pois eu não aguentava de ansiedade e fui arrumar a casa para me distrair, porém fiquei a todo momento de olhos no relógio.\n"
+                        + "\nJá era por volta das 8:30 e minha barriga gritava por comida, então...");
 
         boolean saiu = false;
 
         while (!saiu) {
             System.out.println(
-                    "1 - Decido ir a padaria comer algo.\n2 - Ficar em casa e procurar algo para comer.\n");
+                    "\n1 - Decido ir a padaria comer algo.\n2 - Ficar em casa e procurar algo para comer.\n");
 
             String decisao = sc.next();
 
@@ -135,13 +135,13 @@ public class CenaQuatro {
 
     public static void irAPadaria() {
         Utils.escrever(
-                "Como não havia nada de bom para comer em casa eu decido ir a padaria comer algo, e no caminho eu encontro o Sr.George, que me parecia um tanto quanto preocupado...\n");
+                "Como não havia nada de bom para comer em casa eu decido ir a padaria comer algo, e no caminho eu encontro o Sr.George, que me parecia um tanto quanto preocupado...");
 
         boolean saiu = false;
 
         while (!saiu) {
             System.out.println(
-                    "1 - Conversar com ele e perguntar o que aconteceu.\n2 - Ignorá-lo e ir para padaria.\n");
+                    "\n1 - Conversar com ele e perguntar o que aconteceu.\n2 - Ignorá-lo e ir para padaria.\n");
 
             String decisao = sc.next();
 
@@ -175,7 +175,7 @@ public class CenaQuatro {
 
         while (!saiu) {
             System.out.println(
-                    "1 - Decido ir a padaria comer algo.\n2 - Comer o sanduiche e preparar algo com o ovo e o queijo.\n");
+                    "\n1 - Decido ir a padaria comer algo.\n2 - Comer o sanduiche e preparar algo com o ovo e o queijo.\n");
 
             String decisao = sc.next();
 
@@ -202,7 +202,7 @@ public class CenaQuatro {
 
         while (!saiu) {
             System.out.println(
-                    "1 - Reconsiderar oferta... e irá com senhor Sr.George.\n2 - Recusar novamente e dizer que algo maior esta por vir.\n");
+                    "\n1 - Reconsiderar oferta... e irá com senhor Sr.George.\n2 - Recusar novamente e dizer que algo maior esta por vir.\n");
 
             String decisao = sc.next();
 
@@ -253,7 +253,7 @@ public class CenaQuatro {
 
         while (!saiu) {
             System.out.println(
-                    "1 - Dizer que conhece um rapaz chamado Alan Turing que é muito inteligente e com certeza tem o que é precisa.\n2 - Dizer que infelizmente não conhece ninguém que  possa recomendar.\n");
+                    "\n1 - Dizer que conhece um rapaz chamado Alan Turing que é muito inteligente e com certeza tem o que é precisa.\n2 - Dizer que infelizmente não conhece ninguém que  possa recomendar.\n");
 
             String decisao = sc.next();
 
@@ -277,8 +277,8 @@ public class CenaQuatro {
                 "Disse ao Sr.George que conheço um rapaz chamado Alan Turing que é muito inteligente e que seria capaz de suprir suas expectativas, e que ele era um tanto quanto excêntrico mas que era uma mente de ouro. Ele pega um pequeno caderninho e anota o nome, e diz “Muito obrigado filho estou feliz que você esteja seguindo seu próprio caminho”... E então nos despedimos e eu vou a padaria comer algo. Eu peço o de sempre e após comer eu fico um tempo conversando com a garçonete.... Por volta das 11:40 volto ansioso para casa e me arrumo e 13:20 eu saio de casa e vou andando ate a prefeitura... Ao chegar lá, mostro a carta que recebi e eles me levam a uma sala onde tem vários rapazes mas antes mesmo que eu possa me sentar em uma carteira entra Alan na sala, e vai à frente da sala e diz “Rapazes, por motivos pessoais está prova não irá mais acontecer... Me desculpem pelo ocorrido e peço para que voltem para suas casas e sigam suas vidas.”");
         Utils.escrever(
                 "Após isso fui conversar com Alan e ele me disse que um senhor chamado George Orwell o chamou para uma oportunidade em outro país, sabendo que a vida estava difícil na Inglaterra decidiu largar tudo e ir para lá, pois não tinha mais nada a perder e Alan me agradeceu pela recomendação.\n"
-                        + "E eu fiquei um tanto quanto confuso com aquilo tudo e muito desconcertado ao mesmo tempo, pois perdi duas oportunidades de uma vez só.\n"
-                        + "Horas mais tarde o Sr.George me liga e me diz que Alan contou tudo que havia ocorrido e me perguntou se eu queria ir também, então como não tinha mais opção eu aceitei e fui junto com o Sr.George e com Alan.\n");
+                        + "\nE eu fiquei um tanto quanto confuso com aquilo tudo e muito desconcertado ao mesmo tempo, pois perdi duas oportunidades de uma vez só.\n"
+                        + "\nHoras mais tarde o Sr.George me liga e me diz que Alan contou tudo que havia ocorrido e me perguntou se eu queria ir também, então como não tinha mais opção eu aceitei e fui junto com o Sr.George e com Alan.");
         Utils.escrever(
                 "Fim de jogo.");
 
