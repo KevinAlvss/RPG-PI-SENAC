7e615d5412d2e7b9203bbb40ea07f5084c11b149
Raian Medeiros Nolaço <81272272+RaianNolaco@users.noreply.github.com>
2022-05-10 01:11:38 -0300

    Criação-class-cenaOito

---
+++ Cenas/CenaOito.java
@@ -0,0 +1,15 @@
+package Cenas;
+
+import java.util.Scanner;
+import Utils.Utils;
+import Client.Jogador;
+import Client.Menu;
+
+public class CenaOito {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void initCenaOito() {
+    
+    }
+}
