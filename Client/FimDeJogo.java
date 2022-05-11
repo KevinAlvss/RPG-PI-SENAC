e784190567f5a11543aa93f484d8a844630f8992
Raian Medeiros Nolaço <81272272+RaianNolaco@users.noreply.github.com>
2022-05-11 00:02:39 -0300

    Classe-FimDeJogo-mensagem-final-default

---
+++ Client/FimDeJogo.java
@@ -0,0 +1,15 @@
+package Client;
+
+import Utils.Utils;
+import Client.*;
+
+public class FimDeJogo {
+
+    public static void creditosFimDeJogo(){
+
+        Opcoes.creditos();
+        Utils.escrever("Parabens "+ Jogador.GetNomeJogador() +" Você chegou a um dois finais do jogo\n Muito obrigado por jogar!!! Até a prixima! (._ . )");
+        Menu.exibir();
+    }
+    
+}
