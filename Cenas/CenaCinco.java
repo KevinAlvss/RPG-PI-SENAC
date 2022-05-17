f063b74c6d5a399207afdc6c65bd6057c972e550
Raian Medeiros Nolaço <81272272+RaianNolaco@users.noreply.github.com>
2022-05-17 00:36:56 -0300

    refatorando-formato-das-questoes

--- Cenas/CenaCinco.java
+++ Cenas/CenaCinco.java
@@ -10,6 +10,8 @@ public class CenaCinco {
     static Scanner sc = new Scanner(System.in);
 
     static double pontos;
+    static double resposta;
+    static String respostaEscolha;
 
     public static void initCenaCinco() {
 
@@ -21,30 +23,30 @@ public class CenaCinco {
         Utils.escrever("\n1) Descubra a lógica e complete o próximo elemento:\n");
 
         Utils.escrever("\na) 1, 3, 5, 7...?\n");
-        double respostaA = sc.nextDouble();
+        resposta = sc.nextDouble();
 
-        if (respostaA == 9) {
+        if (resposta == 9) {
             pontos += 1.5;
         }
 
         Utils.escrever("\nb) 2, 4, 8, 16, 32, 64...?\n");
-        double respostaB = sc.nextDouble();
+         resposta = sc.nextDouble();
 
-        if (respostaB == 128) {
+        if (resposta == 128) {
             pontos += 1.5;
         }
 
         Utils.escrever("\nc) 0, 1, 4, 9, 16, 25, 36...?\n");
-        double respostaC = sc.nextDouble();
+        resposta = sc.nextDouble();
 
-        if (respostaC == 49) {
+        if (resposta == 49) {
             pontos += 1.5;
         }
 
         Utils.escrever("\nd) 4, 16, 36, 64...?\n");
-        double respostaD = sc.nextDouble();
+        resposta = sc.nextDouble();
 
-        if (respostaD == 100) {
+        if (resposta == 100) {
             pontos += 1.5;
         }
 
@@ -52,14 +54,11 @@ public class CenaCinco {
                 "\n2) Jogar baralho é uma atividade que estimula o raciocínio. Um jogo tradicional é a Paciência, que utiliza 52 cartas. Inicialmente são formadas sete colunas com as cartas. A primeira coluna tem uma carta, a segunda tem duas cartas, a terceira tem três cartas, a quarta tem quatro cartas, e assim sucessivamente até a sétima coluna, a qual tem sete cartas, e o que sobra forma o monte, que são as cartas não utilizadas nas colunas.\n"
                         + "\nA quantidade de cartas que forma o monte é:\n");
 
-        Utils.escrever("a) 21");
-        Utils.escrever("b) 24");
-        Utils.escrever("c) 26");
-        Utils.escrever("d) 28");
-        Utils.escrever("e) 31");
-        String respostaE = sc.next();
+        Utils.escrever("a) 21\nb) 24\nc) 26\nd) 28\ne) 31");
 
-        if (respostaE.equals("b")) {
+        respostaEscolha = sc.next();
+
+        if (respostaEscolha.equals("b") || respostaEscolha.equals("B")) {
             pontos += 1.5;
         }
 
@@ -71,23 +70,39 @@ public class CenaCinco {
         Utils.escrever("c) 10");
         Utils.escrever("d) 12");
         Utils.escrever("e) 15");
-        String respostaF = sc.next();
+        respostaEscolha = sc.next();
 
-        if (respostaF.equals("a")) {
+        if (respostaEscolha.equals("a") || respostaEscolha.equals("A")) {
             pontos += 1.5;
         }
 
+
+        Utils.escrever("4)Duas tartarugas estavam juntas e começaram a caminhar em linha reta em direção a um lago distante. A primeira tartaruga percorreu 30 metros por dia e demorou 16 dias para chegar ao lago. A segunda tartaruga só conseguiu percorrer 20 metros por dia e, portanto, chegou ao lago alguns dias depois da primeira. Quando a primeira tartaruga chegou ao lago, o número de dias que ela teve que esperar para a segunda tartaruga chegar foi:");
+        
+        Utils.escrever("a) 8");
+        Utils.escrever("b) 10");
+        Utils.escrever("c) 9");
+        Utils.escrever("d) 12");
+        Utils.escrever("e) 15");
+        respostaEscolha = sc.next();
+
+        if (respostaEscolha.equals("a") || respostaEscolha.equals("A")) {
+            pontos += 1.5;
+        }
+
+
+
         Utils.escrever(
-                "\n4) Alguns consideram que a cidade de Florianópolis foi fundada no dia 23 de março de 1726, que caiu em um sábado. Após 90 dias, no dia 21 de junho, a data assinalou o início do inverno, quando a noite é a mais longa do ano. Esse dia caiu em uma:\n");
+                "\n5) Alguns consideram que a cidade de Florianópolis foi fundada no dia 23 de março de 1726, que caiu em um sábado. Após 90 dias, no dia 21 de junho, a data assinalou o início do inverno, quando a noite é a mais longa do ano. Esse dia caiu em uma:\n");
 
         Utils.escrever("a) segunda-feira");
         Utils.escrever("b) terça-feira");
         Utils.escrever("c) quarta-feira");
         Utils.escrever("d) quinta-feira");
         Utils.escrever("e) sexta-feira ");
-        String respostaG = sc.next();
+        respostaEscolha = sc.next();
 
-        if (respostaG.equals("e")) {
+        if (respostaEscolha.equals("e") || respostaEscolha.equals("E")) {
             pontos += 1.5;
         }
 
@@ -112,11 +127,11 @@ public class CenaCinco {
     private static void aprovado() {
 
         Utils.escrever("\nEle chama a 1° pessoa\n"
-                + "\n Joan Clarke....\n"
+                + "\n Joan Clarke com a nota nota mais alta de todas 10.... \n"
                 + "\na segunda pessoa\n"
-                + "\nJack Good\n"
+                + "\nJack Good com a nota 9,5\n"
                 + "\nE antes de chamar a terceira ele diz “Bom não estávamos esperando 3 pessoas mas como foram ótimos resultados\n"
-                + "\nparabéns" + Jogador.GetNomeJogador() + "você ficou com a vaga extra\n");
+                + "\nparabéns " + Jogador.GetNomeJogador() + " com a nota de "+ pontos +", você ficou com a vaga extra exclusiva pra você\n");
 
         Utils.escrever(
                 "\nEntão Alan nos chama para dentro da sala novamente e explica, e antes que Alan comece a dizer algo um rapaz chamado Sr.Stewart nos diz “Meus parabéns sejam bem vindos ao serviço de vossa majestade...Se falarem qualquer coisa que eu dizer aqui, serão executados por traição, mentiram para amigos, família e para qualquer um sobre o que fazem”\n "
