1de1f95b1a3cc571a19065ab8d4ed979ceb32b81
Raian Medeiros Nolaço <81272272+RaianNolaco@users.noreply.github.com>
2022-05-19 17:07:57 -0300

    Complementando-mensagem-final

--- Client/FimDeJogo.java
+++ Client/FimDeJogo.java
@@ -8,6 +8,7 @@ public class FimDeJogo {
 
         Opcoes.creditos();
         Utils.escrever("Parabens "+ Jogador.GetNomeJogador() +" Você chegou a um dois finais do jogo\n Muito obrigado por jogar!!! Até a prixima! (._ . )");
+        Utils.escrever("Caso queira jogar novamente alguma cena vá ao menu ``3 - Selecionsar Cena´´");
         Menu.exibir();
     }
     
